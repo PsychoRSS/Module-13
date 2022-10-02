a9b5f197ca8b16d720146ae847477f99048f4b0a
PsychoRSS <Bobgonzalez516@gmail.com>
2022-10-01 20:02:33 -0600

    second commit

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-router.get('/api/categoreis', (req,res) => {
+// router.get('/api/categoreis', (req,res) => {
   
-})
+// })
 // The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
@@ -11,14 +11,27 @@ router.get('/', async (req, res) => {
     const allcategories = await Category.findAll({
       include: [{ model: Product }],
     });
-    res.status(123).json(allcategories);
+    res.status(200).json(allcategories);
   }catch(err) {
-    res.status(299).json(err);
+    res.status(500).json(err);
   }
   
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const categorytData = await Category.findByPk(req.params.id, {
+    include: [{model:Product}]
+    });
+    
+    if(!categorytDat) {
+      res.status(404).json({ message: 'No traveller found with this id!!!!!!' })
+    }
+    
+    res.status(200).json(categorytDat);
+  }catch (err) {
+    res.status(500).json(err);
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
@@ -33,11 +46,41 @@ router.post('/', (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
+    const categorydata = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+
+    })
+    if (!categorydata[0]) {
+      res.status(404).json({ message: "no Category with this id"});
+      return;
+    }
+    res.status(200).json(categorydata);
+  }catch (err) {
+    res.status(400).json(err)
+  }
   // update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
+  try {
+    const categorydata = Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+    if(!categorydata) {
+      res.status(404).json({message: "No category with this id to delete"})
+      return;
+    }
+    
+    res.status(200).json(categorydata)
+  }catch (err) {
+    res.status(400).json(err)
+  }
   // delete a category by its `id` value
 });
 
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,25 +3,74 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+// Find all Tags
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  try {
+    const allTags = Tag.findAll();
+    res.status(200).json(allTags)
+  }catch(err) {
+    res.status(500).json(err);
+  }
 });
 
+// Find one tag
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  try {
+    const tagData = Tag.findByPk(req.params.id, {
+      include: [{model:Product, as: "Product Data"}]
+    })
+    
+    if(!tagData) {
+      res.status(404).json({ messge: "No item with this id!!!!!!! "})
+    }
+    res.status(200).json(tagData);
+  }catch(err) {
+    res.status(500).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  try{
+    const tagData = Tag.create(req.body);
+    res.status(200).json(tagData)
+  }catch(err) {
+    res.status(400).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  try{
+    const tagData = Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    })
+    
+    if(!tagData[0]) {
+      res.status(404).json({ messge: "No item with this ID!!!!!!!!!!!!!!!!!"})
+      return;
+    }
+    res.status(200).json(tagData);
+  }catch(err) {
+    res.status(400).json(err)
+  }
 });
 
 router.delete('/:id', (req, res) => {
+  try {
+    const tagData = Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+    if(!tagData[0]) {
+      res.status(404).json({ messge: "No item with this ID to delete!!!!"})
+      return;
+    }
+    res.status(200).json(tagData);
+  }catch(err) {
+    res.status(400).json(err)
+  }
   // delete on tag by its `id` value
 });
 
